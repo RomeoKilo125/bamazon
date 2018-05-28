3e5dd983a2c4e5e7108e258088f0f87cf4f9a3a6
RomeoKilo125 <romeokilo125@gmail.com>
2018-05-28 18:36:37 -0400

    completed customer purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,14 +27,63 @@ function pullProducts() {
       productList.push(product.product_name);
     }
     productList.push('');
+    getCustomerChoice(productList);
+  });
+}
+
+function getCustomerChoice(list) {
+  inquirer.prompt([{
+    name: 'productChoice',
+    type: 'list',
+    message: "Here's a list of our products. What would you like to purchase?",
+    choices: list
+  }]).then(answer => {
+    findChosenItem(answer.productChoice);
+  });
+}
+
+function findChosenItem(item) {
+  connection.query('SELECT * FROM products WHERE product_name = "' + item + '"', (error, result) => {
+    let prod = result[0];
     inquirer.prompt([{
-      name: 'productChoice',
-      type: 'list',
-      message: "Here's a list of our products. What would you like to purchase?",
-      choices: productList
+      name: 'purchQuant',
+      type: 'input',
+      message: "You've chosen " + prod.product_name + " ID: " + prod.item_id + "\n" +
+        "Price: $" + prod.price + ".\n" +
+        "There are " + prod.stock_quantity + " available.\n" +
+        "How many would you like to purchase?",
+      default: 1
     }]).then(answer => {
-      console.log(answer);
+      if (answer.purchQuant > prod.stock_quantity) {
+        answer.purchQuant = prod.stock_quantity;
+        inquirer.prompt([{
+          name: 'confirm',
+          type: 'confirm',
+          message: "I'm sorry, we don't have that many in stock.\n" +
+            "Your purchase has been adjusted to the maximum available (" + prod.stock_quantity + ").\n" +
+            "Is this OK?",
+          default: true
+        }]).then(answer => {
+          answer.confirm ? completeTransaction(prod.item_id, prod.stock_quantity, answer.purchQuant) : cancelTransaction();
+        });
+      } else {
+        completeTransaction(prod.item_id, prod.stock_quantity, answer.purchQuant);
+      }
     });
+  });
+}
+
+function completeTransaction(id, q, p) {
+  let queryString = 'UPDATE products SET stock_quantity = ' + (q - p) + ' WHERE item_id = ' + id
+  connection.query(queryString, (error, result) => {
+    if (error) throw error;
+    console.log("Thank you for your purchase!");
+  });
+  closeConnection();
+}
+
+function cancelTransaction() {
+  console.log("We understand. Check back later to see if we have what you're looking for.")
   closeConnection();
 }
 
